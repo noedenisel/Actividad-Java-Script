28198ebf4eed7f3a679a3798cbf8052ed076b69c
Noe Denise <98433663+noedenisel@users.noreply.github.com>
2022-04-04 15:46:49 -0300

    Add files via upload

---
+++ Scipt Practica JS.js
@@ -0,0 +1,56 @@
+function Subrayado ()
+        {
+            if
+            (document.getElementById("text").style.textDecorationLine=="underline")
+            (document.getElementById("text").style.textDecorationLine="none");
+            else
+            (document.getElementById("text").style.textDecorationLine="underline");
+        }
+        function Fondo ()
+        {
+            if
+            (document.getElementById("text").style.backgroundColor=="red")
+            (document.getElementById("text").style.backgroundColor="blue");
+            else 
+            if
+            (document.getElementById("text").style.backgroundColor=="blue")
+            (document.getElementById("text").style.backgroundColor="yellow");
+            else 
+            if
+            (document.getElementById("text").style.backgroundColor=="yellow")
+            (document.getElementById("text").style.backgroundColor="white");
+            else
+            (document.getElementById("text").style.backgroundColor="red");
+        }
+        function texto ()
+        {
+            if
+            (document.getElementById("text").style.color=="red")
+            (document.getElementById("text").style.color="blue");
+            else 
+            if
+            (document.getElementById("text").style.color=="blue")
+            (document.getElementById("text").style.color="yellow");
+            else 
+            if
+            (document.getElementById("text").style.color=="yellow")
+            (document.getElementById("text").style.color="black");
+            else
+            (document.getElementById("text").style.color="red");
+        }
+        function Tamanio ()
+        {
+        if
+        (document.getElementById("text").style.fontSize=="large")
+        (document.getElementById("text").style.fontSize="x-large");
+        else 
+        if
+        (document.getElementById("text").style.fontSize=="x-large")
+        (document.getElementById("text").style.fontSize="xx-large");
+        else 
+        if
+        (document.getElementById("text").style.fontSize=="xx-large")
+        (document.getElementById("text").style.fontSize="medium");
+        else
+        (document.getElementById("text").style.fontSize="large");
+        }
